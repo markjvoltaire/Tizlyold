54f6abb4b76e8b524b923549146a8e9998fab303
Mark Voltaire <markjvoltaire@gmail.com>
2022-11-24 00:25:00 -0500

    general styling

--- components/home/ImagePost.js
+++ components/home/ImagePost.js
@@ -96,7 +96,7 @@ export default function ImagePost({ item, navigation, followingId }) {
   return (
     <>
       <View
-        style={{ paddingBottom: 4, top: height * 0.069, alignSelf: "center" }}
+        style={{ paddingBottom: 4, top: height * 0.04, alignSelf: "center" }}
       >
         <View
           style={{
